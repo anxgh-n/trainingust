667632226aa5812b29da845cfdd7ac1ad4716631
anxgh-n <anaghmanoj@gmail.com>
2024-10-04 10:13:13 +0530

    New commit?

---
+++ demo/src/main/java/com/ust/demo/CompkeyApplication.java
@@ -0,0 +1,13 @@
+package com.ust.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompkeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompkeyApplication.class, args);
+	}
+
+}
